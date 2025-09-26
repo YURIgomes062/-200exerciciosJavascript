685ea9a1304a09bd2fd28d0640bed2d60b891708
Yuri_gomes_062 <yurioficial892@gmail.com>
2025-09-26 02:35:22 -0300

    Exercícios 29.js

---
+++ Exercícios 29.js
@@ -0,0 +1,12 @@
+// Exercício 29: Usando um laço do-while, imprima todos os números pares de 1 a 100.
+
+let num = 0
+
+while( num < 100){
+    num+=2
+    console.log(num)
+}
+
+
+// OBS: se eu colocar na condicional do while " <= " ele irá passar do 100, ele vai até 102.
+// OBS: se eu colocar a variavel num com o valor de 1, Ele vai imprimir até 101.
