6f762446d19483288d57508f7e5fe9f2a2f4bbf8
Yuri_gomes_062 <yurioficial892@gmail.com>
2025-10-04 01:47:21 -0300

    // Exercício 35: Usando um laço for, imprima os primeiros 10 números triangulares (n*(n+1)/2).

---
+++ Exercícios 35.js
@@ -0,0 +1,8 @@
+// Exercício 35: Usando um laço for, imprima os primeiros 10 números triangulares (n*(n+1)/2).
+// O n-ésimo número triangular (T(n)) pode ser calculado usando a seguinte fórmula: T(n) = n(n+1)/2. 
+
+
+for(let i = 1; i <= 10; i++){
+    let result = (i*(i+1)/2)
+    console.log(result)  
+}
