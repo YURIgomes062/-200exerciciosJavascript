6f23d41060e890e58519eb94b05e23347532eb03
Yuri_gomes_062 <yurioficial892@gmail.com>
2025-10-13 23:31:50 -0300

    // Exercício 15: Escreva um programa que verifica se uma palavra é um palíndromo.

--- Exercícios 15.js
+++ Exercícios 15.js
@@ -1,6 +1,7 @@
 // Exercício 15: Escreva um programa que verifica se uma palavra é um palíndromo.
+const prompt = require(`prompt-sync`)()
 
-let word = prompt("Write your word")
+let word = prompt("Write your word");
 
 
   // 2. Converte a string em um array -> ( .split('') ), inverte o array -> ( .reverse() ) , junta novamente em uma string -> ( .join('') )
@@ -8,7 +9,7 @@ let word = prompt("Write your word")
 
 const word_normal = word.split("").reverse().join("");
 
-if(word_nomal == word){
+if(word_normal == word){
     console.log(`Sim a palavra ${word} é um Palíndromo `)
 } else{
     console.log(`Não, a palavra ${word} não é um Palíndromo `)
